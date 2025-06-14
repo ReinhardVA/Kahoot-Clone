6badd42444fd7c11bec167d016128bca4ef79764
Eren Kankılınç <92270328+ReinhardVA@users.noreply.github.com>
2025-06-14 20:44:49 +0300

    Scoring system not works but i guess it's fine

--- socketHandlers/hostHandler.js
+++ socketHandlers/hostHandler.js
@@ -1,14 +1,73 @@
 //vconst socket = import("socket.io")
+const { sessions } = require("../sessionStore");
 
 module.exports = function handleHostEvents(socket, io){
     socket.on('host-join-lobby', (sessionId) => {
         socket.join(sessionId);
-        console.log(`Host joined session ${sessionId}`)
     })
 
-    socket.on('start-question', ({sessionId, question}) => {
-        io.to(sessionId).emit('question', question);
-        console.log(`Question started in session ${sessionId}`);
+    // socket.on('start-question', ({sessionId, question}) => {
+    //     io.to(sessionId).emit('question', question);
+    //     console.log(`Question started in session ${sessionId}`);
+    // });
+
+    socket.on('start-quiz', async ({sessionId, questions}) => {        
+        
+        let currentQuestionIndex = 0;
+
+        const sendNextQuestion = () => {
+            if(currentQuestionIndex >= questions.length){
+                io.to(sessionId).emit("quiz-end");
+                return;
+            }
+            
+            const question = questions[currentQuestionIndex];
+            currentCorrectAnswer = question.correctIndex;
+
+
+            io.to(sessionId).emit("show-question", {
+                question,
+                index: currentQuestionIndex + 1
+            });
+            const answer = {};
+            
+            io.to(sessionId).emit("question-started");
+            // const answerListener = ({sessionId: sId, answer}) => {
+            //     if(sId !== sessionId) return;
+            //     answer[socket.id] = answer;
+            // };
+
+            // socket.on("submit-answer", answerListener);
+
+            setTimeout(() => {
+                //socket.removeListener("submit-answer", answerListener);
+
+                const scoreboard = [];
+
+                const sessionPlayers = sessions[sessionId];
+
+                for(let socketId in sessionPlayers){
+                    const player = sessionPlayers[socketId];
+                    const playerAnswer = player.answer;
+
+                    if(playerAnswer === currentCorrectAnswer){
+                        player.score += 1000;
+                    }
+                    scoreboard.push({
+                        name: player.name,
+                        score: player.score
+                    });
+                    player.answer = null;
+                }
+
+                io.to(sessionId).emit("show-scoreboard", scoreboard);
+                currentQuestionIndex;
+                
+                setTimeout(sendNextQuestion, 5000)
+
+            }, 15000);
+        };
+        sendNextQuestion();
     });
 
 }
--- socketHandlers/playerHandler.js
+++ socketHandlers/playerHandler.js
@@ -1,13 +1,33 @@
 //const socket = import("socket.io")
 
+const sessions = require('../sessionStore')
 module.exports = function handlePlayerEvents(socket, io) {
     socket.on('player-join', ({ sessionId, name}) => {
         socket.join(sessionId);
+        if(!sessions[sessionId]){
+            sessions[sessionId] = {}
+        }
+        sessions[sessionId][socket.Id] = {
+            name,
+            score: 0,
+            answer: null,
+        };
+
         io.to(sessionId).emit('player-joined', name);
-        console.log(`Player ${name} joined session ${sessionId}`);
+        
     });
 
-    socket.on('submit-answer', (data) => {
-        console.log('Answer received:', data)
+    socket.on('submit-answer', ({sessionId, answer}) => {
+        if(sessions[sessionId] && sessions[sessionId][socket.Id]){
+            sessions[sessionId][socket.Id].answer = answer;
+        }
+    });
+
+    socket.on("disconnect", () => {
+        for(let sessionId in sessions) {
+            if(sessions[sessionId][socket.id]){
+                delete sessions[sessionId][socket.id];
+            }
+        }
     })
 }
